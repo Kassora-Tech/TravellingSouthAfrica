13fe6aff76bb0320b8375dccc014dd475d600cec
Dylan Adams <dylan@curaconnect.io>
2026-01-27 02:55:35 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: [
+      "https://6000-firebase-studio-1768999696060.cluster-uf6urqn4lned4spwk4xorq6bpo.cloudworkstations.dev",
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
