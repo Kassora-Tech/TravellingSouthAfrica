2d674acb2a9da4bd2bfadec15098000b0cea1da6
Dylan Adams <dylan@curaconnect.io>
2026-02-25 17:34:27 +0000

    Update the scrolling marquee banner at the top of the homepage only (abo

--- src/components/home/promotional-banner.tsx
+++ src/components/home/promotional-banner.tsx
@@ -4,7 +4,7 @@ import { Translatable } from '@/components/translatable';
 export function PromotionalBanner() {
   const bannerText = "For a limited time, advertise your accommodation, restaurants & must-see services in your town for only R200 per year.";
   return (
-    <Link href="/accommodations#list-accommodation" className="group block w-full overflow-x-hidden bg-blue-900">
+    <Link href="/add-your-listing" className="group block w-full overflow-x-hidden bg-blue-900">
       <div className="flex h-12 items-center">
         <div className="flex animate-marquee whitespace-nowrap group-hover:[animation-play-state:paused]">
           <p className="mx-8 py-3 text-sm font-semibold text-white sm:text-base">
