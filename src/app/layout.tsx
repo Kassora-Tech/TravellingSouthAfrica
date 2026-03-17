369b8ebd411e3cd499c89511090a118859c9b9f8
Dylan Adams <dylan@curaconnect.io>
2026-03-17 17:33:06 +0000

    Install the Google Analytics 4 tracking tag on **every page** of Travell

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,6 +81,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-05SCK85RK9"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-05SCK85RK9');
+            `,
+          }}
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
