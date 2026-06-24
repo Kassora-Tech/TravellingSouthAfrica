bf9462dd4c12dab953ca4c33612836b6b497570a
Chirag <222088255@stu.ukzn.ac.za>
2026-06-24 10:57:49 +0200

    Site Traffic Update 2.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -111,15 +111,18 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <Script
-          id="gtag-base"
+          src="https://www.googletagmanager.com/gtag/js?id=G-PZS244GVLR"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="gtag-init"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-05SCK85RK9');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PZS244GVLR');
             `,
           }}
         />
